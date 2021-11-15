f1b6f7b0552e41c4f96d94ba9e4719b35e3d4a2e
alexx-gsm <alexx-gsm@yandex.ru>
2021-11-15 20:46:49 +0700

    HW_2/ add omit function

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,7 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  return Object.keys(obj).reduce((result, next) => {
+    return fields.includes(next) ? result : Object.assign(result, {[next]: obj[next]});
+  }, {});
 };
